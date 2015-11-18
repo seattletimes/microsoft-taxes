a7275b23c73f876e532b6957272210a879fcfc41
Audrey Carlsen <acarlsen@seattletimes.com>
2015-11-18 14:34:08 -0800

    toggle regions on map

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,9 @@
 
 require("component-responsive-frame/child");
 require("component-leaflet-map");
+var dot = require("./lib/dot");
+
+var template = dot.compile(require("./_infobox.html"));
 
 var mapElement = document.querySelector("leaflet-map");
 var L = mapElement.leaflet;
@@ -35,7 +38,9 @@ var drawLine = function(location1, location2) {
   taxData[region].forEach(function(location) {
     var coords = [location.Lat, location.Lng];
     var marker = L.marker(coords, {
-      icon: L.divIcon()
+      icon: L.divIcon({
+        className: region
+      })
     });
     group.push(marker);
 
@@ -45,7 +50,9 @@ var drawLine = function(location1, location2) {
     }
     prevCoords = coords;
   });
-  regions[region] = L.featureGroup(group);
+
+  var featureGroup = L.featureGroup(group).addTo(map);
+  regions[region] = featureGroup;
 });
 
 var setRegion = function(region) {
@@ -53,15 +60,20 @@ var setRegion = function(region) {
     if (r == region) {
       regions[r].addTo(map);
     } else {
-      map.removeLayer(regions[r]);
+      map.removeLayer(regions[r])
     }
   }
+  document.querySelector(".info-container").innerHTML = template({ hello: "hello" });
 };
 
 qsa(".button").forEach(function(button) {
   button.addEventListener("click", function(e) {
     setRegion(e.target.getAttribute("data-region"));
+    var selected = qsa(".selected.button");
+    if (selected) selected.forEach(function(b) {
+      b.classList.remove("selected");
+    });
+    e.target.classList.add("selected");
   })
 })
 
-setRegion("asia");
